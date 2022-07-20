f1f3afb1eb2974de9b0f8c28ae5a4f4d67f26e16
jiahua6500 <jhua@rbauction.com>
2022-07-20 19:01:55 +0000

    add 1st tag

--- main.tf
+++ main.tf
@@ -129,6 +129,10 @@ resource "aws_instance" "hashicat" {
   tags = {
     Name = "${var.prefix}-hashicat-instance"
   }
+    tags = {
+    Name = "Department"
+    Value = "devops"
+  }
 }
 
 # We're using a little trick here so we can run the provisioner without
